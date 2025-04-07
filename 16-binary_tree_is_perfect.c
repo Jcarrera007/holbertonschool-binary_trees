933167cad506b2f7d0bbe1fb7f449c96529e6062
Jcarrera007 <carrerasproductions@gmail.com>
2025-04-07 17:33:32 -0400

    fix the compile program issue

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,40 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node
- * Return: Height of the tree
- */
-static size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t left_h = 0, right_h = 0;
-
-	if (tree == NULL)
-		return (0);
-
-	left_h = binary_tree_height(tree->left);
-	right_h = binary_tree_height(tree->right);
-
-	return (1 + (left_h > right_h ? left_h : right_h));
-}
-
-/**
- * binary_tree_size - Counts total nodes in a binary tree
- * @tree: Pointer to the root node
- * Return: Number of nodes
- */
-static size_t binary_tree_size(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-
-	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
-}
-
-/**
  * binary_tree_is_perfect - Checks if a binary tree is perfect
  * @tree: Pointer to the root node
- * Return: 1 if tree is perfect, 0 otherwise
+ *
+ * Return: 1 if perfect, 0 otherwise or if tree is NULL
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {

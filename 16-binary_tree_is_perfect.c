609b8ad1b29fc6691486efd96a72d544b9fa6a8a
Jcarrera007 <carrerasproductions@gmail.com>
2025-04-09 13:38:42 -0400

    ..

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -37,11 +37,11 @@ size_t binary_tree_size(const binary_tree_t *tree)
  * binary_tree_is_perfect - Checks if a binary tree is perfect
  * @tree: Pointer to the root node
  *
- * Return: 1 if perfect, 0 otherwise or if tree is NULL
+ * Return: 1 if perfect, 0 otherwise
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t height, size, expected_nodes;
+	size_t height, expected_nodes, actual_nodes;
 
 	if (tree == NULL)
 		return (0);
@@ -52,3 +52,4 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	return (actual_nodes == expected_nodes);
 }
+
